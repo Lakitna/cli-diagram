26bf28462f73b7db3804bba6bc1e297fed4bc90d
George Pickering <29524044+geopic@users.noreply.github.com>
2020-02-25 15:36:25 +0000

    Add types declaration file with Diagram class

---
+++ src/index.d.ts
@@ -0,0 +1,15 @@
+declare module 'cli-diagram' {
+  class Diagram extends Array {
+    constructor(options?: object);
+
+    draw(): string;
+    height: number;
+    box(contents: string, options?: object): Diagram;
+    container(contents: string, options?: object): Diagram;
+    lines(lines: number | string[], options?: object): Diagram;
+    arrow(lines: number | string[], options?: object): Diagram;
+    space(size?: number): Diagram;
+  }
+
+  export = Diagram;
+}
